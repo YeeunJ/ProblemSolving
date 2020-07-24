4565adc5c2af1244cd15af9b8da96a7bc1f711b3
YeeunJ <21800669@handong.edu>
2020-07-25 01:41:02 +0900

    N3663 explain

--- src/solving/problem/java/c/N3663.java
+++ src/solving/problem/java/c/N3663.java
@@ -21,23 +21,24 @@ public class N3663 {
 			for(int j=0; j< len; j++) {
 				sum += Math.min(str.charAt(j) - 'A', 26 - (str.charAt(j) - 'A'));
 				
-				if(str.charAt(j) == 'A' && flag == -1) {
+				if(str.charAt(j) == 'A' && flag == -1) { //처음 A가 나왔을때
 					flag = j;
 					count++;
-				}else if(str.charAt(j) == 'A') {
+				}else if(str.charAt(j) == 'A') { //A가 반복될
 					count++;
-				}else if(flag == 0) {
+				}else if(flag == 0) { //앞 AA
 					min = Math.min(min, (len-count));
 					flag = -1;
 					count = 0;
 				}
-				else if(flag != -1) {
+				else if(flag != -1) { //A부분이 끝났을때!!
 					min = Math.min(min, (len-count-1 + Math.min(len - j, flag -1)));
 					flag = -1;
 					count = 0;
 				}
 			}
-			if(flag != -1 && sum != 0) {
+			
+			if(flag != -1 && sum != 0) { //맨 마지막 AA
 				min = Math.min(min, flag-1);
 			}else if(sum == 0) {
 				min = 0;
